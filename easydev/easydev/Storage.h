7b70d3fad06e7be90e80b205e37ebed940badc07
Kim, Hyuk <hyukya@gmail.com>
2019-07-09 22:00:46 +0900

    add new interface

--- easydev/easydev/Storage.h
+++ easydev/easydev/Storage.h
@@ -3,9 +3,16 @@
 
 class Storage {
 public:
-	virtual void begin_tx() = 0;
-	virtual void end_tx() = 0;
+	virtual void BeginTransaction() = 0;
+	virtual void EndTransaction() = 0;
 
-	virtual bool push_back(const DataType&) = 0;
-	virtual bool pop_front(DataType&) = 0;
+	// space size of Storage
+	virtual uint64_t Size() = 0;
+	// DataType counts in Storage
+	virtual uint64_t Count() = 0;
+
+	// interface for queue
+	virtual void Push(const DataType&) = 0;
+	virtual const DataType& Front() = 0;
+	virtual void Pop() = 0;
 };
