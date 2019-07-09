40f244f794be348348d19d27fb476fd2d5ba8d8a
Kim, Hyuk <hyukya@gmail.com>
2019-07-09 21:47:53 +0900

    Storage uses DataType and remove DataStorage class

--- easydev/easydev/Storage.h
+++ easydev/easydev/Storage.h
@@ -1,10 +1,11 @@
 #pragma once
+#include "DataType.h"
 
 class Storage {
 public:
 	virtual void begin_tx() = 0;
 	virtual void end_tx() = 0;
 
-	virtual bool push_back(void* data) = 0;
-	virtual bool pop_front(void** data) = 0;
+	virtual bool push_back(const DataType&) = 0;
+	virtual bool pop_front(DataType&) = 0;
 };
