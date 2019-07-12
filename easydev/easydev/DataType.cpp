e4f71618184d9c435485adc8300c385be851e220
Kim, Hyuk <hyukya@gmail.com>
2019-07-13 00:38:59 +0900

    DataType makes DataAttribute itself

--- easydev/easydev/DataType.cpp
+++ easydev/easydev/DataType.cpp
@@ -11,3 +11,38 @@ std::string DataType::GetName()
 {
 	return this->data_attribute_.GetName();
 }
+
+DataType* DataType::GetInstance(const DATA_TYPE& data_type)
+{
+	std::string name;
+	uint32_t size;
+	
+	switch (data_type)
+	{
+	case DATA_TYPE::UINT32_T:
+		name = "uint32_t";
+		size = 4;
+		break;
+	case DATA_TYPE::INT32_T:
+		name = "int32_t";
+		size = 4;
+		break;
+	case DATA_TYPE::UINT64_T:
+		name = "uint64_t";
+		size = 8;
+		break;
+	case DATA_TYPE::INT64_T:
+		name = "int64_t";
+		size = 8;
+		break;
+	case DATA_TYPE::STRING:
+		name = "string";
+		size = 0;
+		break;
+	default:
+		break;
+	}
+	DataAttribute data_attribute(name, size);
+
+	return new DataType(data_attribute);
+}
--- easydev/easydev/DataType.h
+++ easydev/easydev/DataType.h
@@ -4,11 +4,23 @@
 
 class DataType {
 public:
-	DataType(const DataAttribute& data_attribute);
+	enum class DATA_TYPE {
+		UINT32_T,
+		INT32_T,
+		UINT64_T,
+		INT64_T,
+		STRING,
+		END_DATA_TYPE
+	};
+public:
 	virtual ~DataType();
+
+	static DataType* GetInstance(const DATA_TYPE& data_type);
 
 	std::string GetName();
 
+private:
+	DataType(const DataAttribute& data_attribute);
 private:
 	DataAttribute data_attribute_;
 };
